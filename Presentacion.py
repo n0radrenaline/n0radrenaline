ea8f545159ce07a05eff25860f66a135d81b2e16
n0radrenaline <149545100+n0radrenaline@users.noreply.github.com>
2024-02-15 01:16:54 -0300

    Presentacion.py
    mi proyecto abarcara múltiples asuntos los cuales pueden tener errores siéntete libre de comunicármelo

---
+++ Presentacion.py
@@ -0,0 +1,5 @@
+print(hola)
+Me presento soy @retr00 me gusta mucho la ciberseguridad, anilisis de datos, IA, ciencia de datos y python 
+La verdad vengo a proponerles algo yo plasmare todo mi conocimiento de todo lo que vaya aprendiendo y lo que he aprendido 
+busco que una empresa financie mi educacion 
+si te gustaria apoyar al proyecto lo puedes hacer atraves de worldcoin o cualquier otra criptomoneda
